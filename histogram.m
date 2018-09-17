0258a927c314f6b728cf97774ff08445d590fc7b
nnsnorth <33376603+nnsnorth@users.noreply.github.com>
2018-09-18 03:51:21 +0700

    Update histogram.m

--- histogram.m
+++ histogram.m
@@ -10,6 +10,8 @@ eq_tiff = histeq(tiff);
 eq_png = histeq(png);
 eq_gif = histeq(gif);
 
+%%
+%histogram analytic
 subplot(5,2,1);
 imshow(bmp);
 subplot(5,2,2);
@@ -34,3 +36,30 @@ subplot(5,2,9);
 imshow(gif);
 subplot(5,2,10);
 imhist(gif);
+%%
+%histogram equalization
+figure;
+subplot(5,2,1);
+imshow(eq_bmp);
+subplot(5,2,2);
+imhist(eq_bmp);
+
+subplot(5,2,3);
+imshow(eq_jpg);
+subplot(5,2,4);
+imhist(eq_jpg);
+
+subplot(5,2,5);
+imshow(eq_tiff);
+subplot(5,2,6);
+imhist(eq_tiff);
+
+subplot(5,2,7);
+imshow(eq_png);
+subplot(5,2,8);
+imhist(eq_png);
+
+subplot(5,2,9);
+imshow(eq_gif);
+subplot(5,2,10);
+imhist(eq_gif);
